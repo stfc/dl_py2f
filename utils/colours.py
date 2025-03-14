7d8a3a5a83294f6f608fd1fcef6e6b3bae58c41e
You Lu <you.lu@stfc.ac.uk>
2025-03-14 03:04:43 +0000

    Inserted licence statements

--- utils/colours.py
+++ utils/colours.py
@@ -1,3 +1,22 @@
+#  Copyright (C) 2025 The authors of DL_PY2F
+#
+#  This file is part of DL_PY2F.
+#
+#  DL_PY2F is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  DL_PY2F is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with DL_PY2F. If not, see
+#  <https://www.gnu.org/licenses/>.
+
+# you.lu@stfc.ac.uk
 _u_W_ = '\033[4;37;40m'
 _E     = '\033[0;30;40m'
 _bE    = '\033[0;90;40m'
